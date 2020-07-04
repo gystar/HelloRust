9462670f92c8f08ac5959406120cb8c6323e9a6c
gitstar <gy163email@163.com>
2020-07-04 19:41:14 +0800

    7.4下午学习日志，基本完成编程之道第二章的学习

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
 extern crate std;
 
 fn main() {
-    println!("Hello,guiyi！");
-    pub fn answer() -> () {
-        let a = 1;
-        let b = 2;
-        assert_eq!(sum(a, b), 3);
-    }
-    pub fn sum(a: i32, b: i32) -> i32 {
-        a + b
-    }
-    answer();
+    println!("{:?}", 2);
 }
