61ff846b89528b99d87116610260659da606d698
gitstar <gy163email@163.com>
2020-07-04 11:48:57 +0800

    7.4上午的学习笔记

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+extern crate std;
+
 fn main() {
     println!("Hello,guiyi！");
+    pub fn answer() -> () {
+        let a = 1;
+        let b = 2;
+        assert_eq!(sum(a, b), 3);
+    }
+    pub fn sum(a: i32, b: i32) -> i32 {
+        a + b
+    }
+    answer();
 }
