7bfc75dab479d7567e9cc604e668801ffea91452
gitstar <gy163email@163.com>
2020-07-06 19:40:13 +0800

    修正一个集成测试的bug，需要提前创建文件夹，否则文件会创建失败

--- csv_challenge/tests/integration_test.rs
+++ csv_challenge/tests/integration_test.rs
@@ -1,15 +1,17 @@
 #[cfg(test)]
 mod test {
     use csv_challenge::{
-        replace_column, Opt, {load_csv, write_csv},
+        replace_column, {load_csv, write_csv},
     };
     use std::path::PathBuf;
     #[test]
     fn test_csv_challenge() {
         let filename = PathBuf::from("./input/1.csv");
         let csv_data = load_csv(filename).unwrap();
+        println!("{}", csv_data);
         let modified_data = replace_column(csv_data, "City", "Beijing").unwrap();
-        let output_file = write_csv(&modified_data, "ouput/test.csv");
+        println!("{}", modified_data);
+        let output_file = write_csv(&modified_data, "output/test.csv");
         assert!(output_file.is_ok());
     }
 }
